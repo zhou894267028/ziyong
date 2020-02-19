babf7b2295382ebd4586efa70d266199aca3ace2
zhou894267028 <37473174+zhou894267028@users.noreply.github.com>
2020-02-19 15:29:27 +0800

    修改注释

--- ssr.sh
+++ ssr.sh
@@ -6,8 +6,8 @@ export PATH
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
 #	Description: Install the ShadowsocksR server
 #	Version: 2.0.38
-#	Author: Toyo
-#	Blog: https://doub.io/ss-jc42/
+#	Author: zhou
+#	Blog: http://wenziju.com
 #=================================================
 
 sh_ver="2.0.38"
